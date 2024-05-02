8315b41e6899220ff756471fdc32e8709d59e4a7
sonei666888 <1144284812@qq.com>
2024-05-03 00:57:08 +0800

    append

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ http://localhost:8083/#/
 
 axios version
 npm install axios@1.5.0 --save
+
+格式化
+Shift + Alt + F
+
+
+vue-router
+npm install vue-router@3 
+vue2-----vue-router3
+vue3-----vue-router4
--- src/main.js
+++ src/main.js
@@ -7,13 +7,14 @@ import 'element-ui/lib/theme-chalk/index.css';
 import 'font-awesome/css/font-awesome.min.css'
 import router from './router'
 import axios from 'axios'
- 
+import VueRouter from 'vue-router';
 
 Vue.config.productionTip = false
 //将包挂载到vue的原型对象上
 Vue.prototype.$axios = axios
 axios.defaults.baseURL='http://localhost:8080'
 
+Vue.use(VueRouter);
 Vue.use(ElementUI);
 
 /* eslint-disable no-new */
